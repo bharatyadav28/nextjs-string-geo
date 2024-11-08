2f769ebb4088292acac4a9df5a30c1111b935378
Bharat Yadav <yadavbharat386@gmail.com>
2024-11-08 17:51:05 +0530

    Updated the standard width and height of image  for whatsapp

--- src/app/(main)/free-video/[id]/page.js
+++ src/app/(main)/free-video/[id]/page.js
@@ -1,67 +1,55 @@
 import React from "react";
-import { headers } from "next/headers";
-
 import VideoPage from "@/ClientPages/VideoPage";
 
 // Generate metadata for the video page
-export async function generateMetadata({ params }, parent) {
+export async function generateMetadata({ params }) {
+  // Fetch the video data
   const rVideo = await fetch(
     `https://api.stringgeo.com/api/free-video/get-video/${params.id}`
   );
-
   const { video } = await rVideo.json();
 
-  console.log("videodatataa", video);
-
+  // Thumbnail URL Generated
   const thumbnailUrl = `https://dewv7gdonips4.cloudfront.net/${video.thumbnail_url}`;
-  const fallbackImageUrl =
-    "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
 
-  console.log("thumbnail_url", thumbnailUrl);
   return {
     title: video.title.substring(0, 100),
     description: video.description,
     openGraph: {
+      type: "video.other",
       title: video.title,
       description: video.description,
       images: [
         {
           url: thumbnailUrl,
-          width: 1920,
-          height: 1080,
-          type: "image/webp",
-          alt: video.title,
-        },
-        {
-          url: fallbackImageUrl,
-          width: 272,
-          height: 92,
-          alt: "Google Search",
+          width: 1200, // Set standard width
+          height: 630, // Set standard height
         },
       ],
-      // Specify the type as 'video.other' for video content
-      type: "video.other",
+      url: `https://your-domain.com/video/${video.id}`,
       videos: [
         {
-          url: video.video_url, // Add if you want to enable video preview
+          url: video.video_url,
           width: 1280,
           height: 720,
           type: "video/mp4",
         },
       ],
     },
-    // Twitter Card metadata
     twitter: {
-      card: "summary_large_image",
+      card: "player",
       title: video.title,
       description: video.description,
       images: [thumbnailUrl],
+      player: video.video_url,
+      playerWidth: 1280,
+      playerHeight: 720,
     },
   };
 }
 
 async function Page({ params }) {
-  const id = (await params).id;
+  const id = params.id;
   return <VideoPage id={id} />;
 }
 
